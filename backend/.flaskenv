c4f7abc6df08a926ae69b2509140eb7a0e6902ab
partheev <partheev8@gmail.com>
2023-08-13 19:38:14 +0530

    feat(campus-api): access excel file from post request
    - add cors

--- backend/.flaskenv
+++ backend/.flaskenv
@@ -1,2 +1,3 @@
 export FLASK_ENV=development
 export FLASK_APP=src
+export FLASK_DEBUG=1
--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,13 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    CORS(app)
 
-    @app.get("/")
-    def home():
+    @app.get('/')
+    def Check():
         return {
             'status': 'Working....'
+        }
+
+    @app.post("/api/predict-campus-placements")
+    @cross_origin(origins='*')
+    def PredictCampusPlacements():
+        print('hello')
+        campus_data_file = request.files['file']
+
+        return {
+            'status': 'file uploaded....',
         }
 
     return app
