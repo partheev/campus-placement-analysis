2ac9ea4e048856074c8558c795b364a7f9bfa6ff
poornesh <poorneshchenna29@gmail.com>
2023-08-13 19:03:38 +0530

    feat : navbar added

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from 'react'
+import DesktopView from './DesktopView'
+import MobileView from './MobileView'
+
 const PAGES = [
-    {
-        name: 'Campus Analyzer',
-        path: '/campus-placement-analyzer',
-    },
-    {
-        name: 'Student Placement Analyzer',
-        path: '/student-placement-analyzer',
-    },
-];
+  {
+    name: 'Campus Analyzer',
+    path: '/campus-placement-analyzer',
+  },
+  {
+    name: 'Student Placement Analyzer',
+    path: '/student-placement-analyzer',
+  },
+]
 export const Header = () => {
-    return <>Header</>;
-};
+  const [isMobile, setIsMobile] = useState(false)
+
+  const handleResize = () => {
+    if (window.innerWidth < 720) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }
+
+  // create an event listener
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+  })
+  return <>{!isMobile ? <DesktopView /> : <MobileView />}</>
+}
