859da01dc2beb4db0e5d1948c5f55616683472c7
partheev <partheev8@gmail.com>
2023-08-26 14:31:28 +0530

    chore(env): backend_url add to env in frontend

--- frontend/src/apis/configs/axiosConfig.js
+++ frontend/src/apis/configs/axiosConfig.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // initializing the axios instance with custom configs
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     timeout: 10000,
 });
+console.log(import.meta.env.VITE_BACKEND_URL);
 
 export default api;
