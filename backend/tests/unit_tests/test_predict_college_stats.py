2ebfd2275807fe9378c50bb319b0eb254eb3eaf4
Nomika <nomigajula412@gmail.com>
2023-08-26 17:25:31 +0530

    dict to json conversion

--- backend/tests/unit_tests/test_predict_college_stats.py
+++ backend/tests/unit_tests/test_predict_college_stats.py
@@ -2,11 +2,15 @@
 
 from src.ml.predict import predict_college_stats
 # from src.ml.prediction_models import get_data
+import json
 def test_predict_college_stats():
 
     with open('tests/unit_tests/sample_format.csv', 'r') as excel_sheet:
          stats=predict_college_stats(excel_file=excel_sheet)
          print(stats)
+         Annotation_final_json = json.dumps(stats)
+         print(Annotation_final_json)
+         
     
        
 test_predict_college_stats()
