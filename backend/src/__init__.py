02459e323d56a2a8691876bfcd2edd29c56b653c
partheev <partheev8@gmail.com>
2023-08-26 12:38:35 +0530

    feat(api): 400 response if file not found

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
+from src.ml.predict import predict_college_stats
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -16,10 +18,16 @@ def create_app(test_config=None):
     @cross_origin(origins='*')
     def PredictCampusPlacements():
         print('hello')
-        campus_data_file = request.files['file']
-
+        campus_data_file = request.files.get('file', None)
+        if campus_data_file is None:
+            return {
+                'message': '[file] key not found in the form-data. Please upload excel file to fetch insights.'
+            }, 400
+        stats = predict_college_stats(campus_data_file)
+        print(stats)
         return {
             'status': 'file uploaded....',
+            'stats': stats
         }
 
     return app
