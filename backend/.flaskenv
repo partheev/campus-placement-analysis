af3c3a4a926728142efea387f45a27cf0518b862
partheev <partheev8@gmail.com>
2023-08-26 11:42:24 +0530

    feat(ml): add  ml model api from ibm cloud

--- backend/.flaskenv
+++ backend/.flaskenv
@@ -1,3 +1,4 @@
 export FLASK_ENV=development
 export FLASK_APP=src
 export FLASK_DEBUG=1
+export USE_DEPLOYED_ML_MODEL=1
---
+++ backend/src/ml/ml_deploy.py
@@ -0,0 +1,102 @@
+import requests
+import os
+
+API_KEY = os.getenv('ML_DEPLOYMENT_API_KEY')
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+                                                                                 API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+print("Model model connected.")
+
+"""
+input_fields - array of fields
+ [
+          "tier",
+          "cgpa",
+          "inter_gpa",
+          "ssc_gpa",
+          "internships",
+          "no_of_projects",
+          "is_participate_hackathon",
+          "is_participated_extracurricular",
+          "no_of_programming_languages",
+          "dsa",
+          "mobile_dev",
+          "web_dev",
+          "Machine Learning",
+          "cloud",
+          "CSE",
+          "ECE",
+          "IT",
+          "MECH"
+  ]
+
+  feature_values - array of arrays. Each array in the array contains features.
+"""
+
+
+def predict_salary_api(feature_values):
+
+    header = {'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + mltoken}
+
+    # NOTE: manually define and pass the array(s) of values to be scored in the next line
+    payload_scoring = {"input_data": [{"fields": [
+        "tier",
+        "cgpa",
+        "inter_gpa",
+        "ssc_gpa",
+        "internships",
+        "no_of_projects",
+        "is_participate_hackathon",
+        "is_participated_extracurricular",
+        "no_of_programming_languages",
+        "dsa",
+        "mobile_dev",
+        "web_dev",
+        "Machine Learning",
+        "cloud",
+        "CSE",
+        "ECE",
+        "IT",
+        "MECH"
+    ], "values": feature_values}]}
+
+    response_scoring = requests.post('https://eu-gb.ml.cloud.ibm.com/ml/v4/deployments/e91a8129-b756-4fd3-8533-b22f4219d8bf/predictions?version=2021-05-01', json=payload_scoring,
+                                     headers={'Authorization': 'Bearer ' + mltoken})
+
+    return response_scoring
+
+
+def predict_isplaced_api(feature_values):
+
+    header = {'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + mltoken}
+
+    # NOTE: manually define and pass the array(s) of values to be scored in the next line
+    payload_scoring = {"input_data": [{"fields": [
+        "tier",
+        "cgpa",
+        "inter_gpa",
+        "ssc_gpa",
+        "internships",
+        "no_of_projects",
+        "is_participate_hackathon",
+        "is_participated_extracurricular",
+        "no_of_programming_languages",
+        "dsa",
+        "mobile_dev",
+        "web_dev",
+        "Machine Learning",
+        "cloud",
+        "CSE",
+        "ECE",
+        "IT",
+        "MECH"
+    ], "values":feature_values}]}
+
+    response_scoring = requests.post('https://eu-gb.ml.cloud.ibm.com/ml/v4/deployments/e91a8129-b756-4fd3-8533-b22f4219d8bf/predictions?version=2021-05-01', json=payload_scoring,
+                                     headers={'Authorization': 'Bearer ' + mltoken})
+
+    return response_scoring
