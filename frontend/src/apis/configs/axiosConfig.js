e9d667ad50fed0abe318c7cd772771f9db34becc
partheev <partheev8@gmail.com>
2023-08-28 12:06:18 +0530

    chore: production url add

--- frontend/src/apis/configs/axiosConfig.js
+++ frontend/src/apis/configs/axiosConfig.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 
 // initializing the axios instance with custom configs
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: import.meta.env.VITE_BACKEND_URL + '/api',
     timeout: 10000,
 });
-console.log(import.meta.env.VITE_BACKEND_URL);
 
 export default api;
